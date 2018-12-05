359bea057d74af2952580adaa922bfc63bb4c22c
Yin1012 <43611713+Yin1012@users.noreply.github.com>
2018-12-05 13:49:54 -0500

    delete unnessesary test file

--- tests/testthat/test_Viz.R
+++
@@ -1,9 +0,0 @@
-context("Viz")
-# ==== BEGIN SETUP AND PREPARE ==========================================
-library(chromoMap)
-devtools::install_github("MangoTheCat/visualTest")
-library(visualTest)
-# ==== END SETUP AND PREPARE ========================================
-test_that("The possible pairs are identified by visualization reslult of heat map")
-
-test_that("The possible reads are identified by visualization reslult of annotation")
